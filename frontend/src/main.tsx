9763987e7f1c96aca356d72ad9c26a72857a5c38
kiyun <dagulminzz@naver.com>
2026-08-09 15:44:37 +0900

    feat(frontend): MSW mock API 환경 구성

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,22 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+  return;
+}
+
+const { worker } = await import('./mocks/browser');
+
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+    </React.StrictMode>,
+  );
+});
